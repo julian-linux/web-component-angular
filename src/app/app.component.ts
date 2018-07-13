2dfe7b005c0bdfe47458b25ea5f8488be74db3c7
Julian Mosquera <julian.linux@gmail.com>
2018-07-13 15:01:37 -0500

    1st commit. I can register my cell phone in twitter... so i cant use the api. At this points moments are mocks.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,55 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import TweetService from './tweet.service';
+// import { AppModule } from './app.module';
+import {AppInjector} from './app.injector';
 @Component({
-  selector: 'ngx-root',
+  selector: 'ngx-tweet',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: [
+    "./bootstrap.min.css",
+  ],
+  encapsulation: ViewEncapsulation.Native
 })
-export class AppComponent {
-  title = 'ngx';
+export class AppComponent implements OnInit{
+  searchText: string;
+  order: string;
+  list: string[];
+  private tweetService;
+  constructor(
+  ){
+    this.tweetService = AppInjector.get(TweetService);
+    
+  }
+
+  ngOnInit() {
+    this.order = "asc";
+    this.list = [];
+    this.searchText = "";
+    this.getTweets();
+  }
+
+  getTweets() {
+    console.log("tweets");
+    this.list = this.tweetService.getTweets(this.searchText, this.order);
+  }
+  
+  // onSubmit(evt:any) {
+  //   console.log("submit", evt);
+  //   this.getTweets();
+  //   return false;
+  // }
+
+  onKey(evt:any, value:string) {
+    console.log("key");
+    this.searchText = value;
+    if(evt.keyCode === 13) {
+      this.getTweets();
+    }
+  }
+  
+  changeOrder(dir:string) {
+    console.log("order");
+    this.order = dir;
+    this.getTweets();
+  }
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,28 @@
 import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { NgModule, Injector } from '@angular/core';
 
+import { createCustomElement } from '@angular/elements';
+import {setAppInjector} from './app.injector';
 import { AppComponent } from './app.component';
 
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
-  imports: [
-    BrowserModule
-  ],
+  declarations: [AppComponent],
+  imports: [BrowserModule],
   providers: [],
-  bootstrap: [AppComponent]
+  entryComponents: [AppComponent],
+  // bootstrap: [AppComponent],
 })
-export class AppModule { }
+
+export class AppModule {
+  static injector: Injector;
+
+  constructor(private injector: Injector) {
+    setAppInjector(this.injector);
+  }
+  
+  ngDoBootstrap() { 
+    const tweets = createCustomElement(AppComponent, { injector: this.injector });
+    customElements.define('ngx-tweet', tweets);
+
+  }
+}
